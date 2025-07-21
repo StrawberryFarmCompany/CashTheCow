ae1313656c4fddd7f5a5c6a60c0de2aff1a1a04a
Heoyoon <parechae@gmail.com>
2025-07-22 02:44:18 +0900

    feat : DoTween 설치 및 슬롯머신 연출 구현
    스페이스바를 누르세용

--- Assets/01.SlotMachine/Scripts/SlotDirrector.cs
+++ Assets/01.SlotMachine/Scripts/SlotDirrector.cs
@@ -1,34 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using System;
 
 public class SlotDirrector : MonoBehaviour
 {
-    public Sprite[] samples;
-    public Transform[] slots;
-    public Image[] targetImages;
+    [SerializeField]private Sprite[] samples;
+    [SerializeField] private Image[] dirrSlot;
+    [SerializeField] private Image[] resultImages;
+
+    [SerializeField] private int[] results;
+    [SerializeField] private float height;
     void Start()
     {
-        
+        height = resultImages[0].rectTransform.sizeDelta.y;
     }
 
-    void ReceiveDirrection()
+    void ReceiveDirrection(int index)
     {
-
+        int temp = new System.Random().Next(1, 5);
+        StartCoroutine(SwapAnims(index, temp,3f));
+        
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            results[0] = UnityEngine.Random.Range(0, samples.Length);
+            results[1] = UnityEngine.Random.Range(0, samples.Length);
+            results[2] = UnityEngine.Random.Range(0, samples.Length);
+            ReceiveDirrection(0);
+            ReceiveDirrection(1);
+            ReceiveDirrection(2);
         }
     }
-    public IEnumerator SwapAnims()
+    public IEnumerator SwapAnims(int index, int loopTimes,float time)
     {
-        yield return null;
+        int offSet = results[index]+(loopTimes * samples.Length);
+        int currIndex = 0;
+        int idxOne = index * 2;
+        int idxTwo = index * 2 + 1;
+
+        resultImages[index].rectTransform.localPosition = Vector3.zero;
+
+        Sequence firstSequences = DOTween.Sequence();
+        Sequence secondSequences = DOTween.Sequence();
+        float timePerLoop = time / loopTimes;
+        firstSequences.Pause();
+        secondSequences.Pause();
+        while (offSet > 0)
+        {
+
+            offSet -=2;
+            int firstSample = currIndex% samples.Length;
+            int secondSample = (currIndex + 1) % samples.Length;
+            currIndex+=2;
+
+            float tempHei = height * -2f;//
+
+            if (offSet <= 0) break;
+
+            firstSequences.Append(dirrSlot[idxOne].rectTransform.DOLocalMoveY(tempHei, 0.3f));
+
+            secondSequences.Append(dirrSlot[idxTwo].rectTransform.DOLocalMoveY(tempHei, 0.3f));
+
+            firstSequences.AppendCallback(() => { dirrSlot[idxOne].rectTransform.localPosition = Vector3.zero; dirrSlot[idxOne].sprite = samples[firstSample]; });
+
+            secondSequences.AppendCallback(() => { dirrSlot[idxTwo].rectTransform.localPosition = Vector3.zero; dirrSlot[idxTwo].sprite = samples[secondSample]; });
+
+        }
+        resultImages[index].sprite = samples[results[index]];
+        firstSequences.Append(resultImages[index].rectTransform.DOLocalMoveY(-height,0.3f));
+        firstSequences.Play();
+        yield return new WaitForSeconds(0.15f);
+        secondSequences.Play();
+        secondSequences.Play();
     }
     //최종루프엔 
 }
